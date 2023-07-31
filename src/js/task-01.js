2382337ffd56f3dcdb5559a711d9d0802d9e43e1
Tamias <n.tamia5@icloud.com>
2023-08-01 00:02:54 +0300

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,13 +1,11 @@
 const categoriesList = document.querySelector('#categories');
-
 const categories = categoriesList.querySelectorAll('.item');
 
 console.log('Number of categories:', categories.length);
 
 categories.forEach((category) => {
     const categoryName = category.querySelector('h2').textContent;
-    const elements = category.querySelectorAll('li');
-
     console.log('Category:', categoryName);
-    console.log('ELements:', elements.length);
+    const elements = category.querySelectorAll('li');
+    console.log('Elements:', elements.length);
 })
