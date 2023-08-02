9db9477656baa6e626f49a0d753cae6ecc241e04
Tamias <n.tamia5@icloud.com>
2023-08-02 19:55:51 +0300

    task 02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -7,3 +7,13 @@ const ingredients = [
   'Condiments',
 ];
 
+const ingredientsList = document.getElementById('ingredients');
+
+ingredients.forEach((ingredient) => {
+  const liEl = document.createElement('li');
+  liEl.textContent = ingredient;
+  // console.log(liEl);
+
+  liEl.classList.add('item');
+  ingredientsList.appendChild(liEl);
+})
