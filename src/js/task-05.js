5a88b82d07ce8a6706f048d09f70f976c0e85d55
Tamias <n.tamia5@icloud.com>
2023-08-07 15:19:11 +0300

    task 05 and 06

---
+++ src/js/task-05.js
@@ -0,0 +1,9 @@
+const nameInput = document.getElementById('name-input');
+const nameOutput = document.getElementById('name-output');
+
+nameInput.addEventListener('input', () => {
+
+    const inputValue = nameInput.value;
+
+    nameOutput.textContent = inputValue ? inputValue : 'Anonymous';
+});
