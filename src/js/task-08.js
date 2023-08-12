2d2d2ef96c0adc8c1d1babee92b18f973b209cb6
Tamias <n.tamia5@icloud.com>
2023-08-12 21:32:41 +0300

    task 08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const inputMail = loginForm.elements.email;
+    const inputPassword = loginForm.elements.password;
+
+    if (!inputMail.value || !inputPassword.value) {
+        alert('Всі поля повинні бути заповнені');
+    } else {
+        const data = {
+            email: inputMail.value,
+            password: inputPassword.value
+        };
+        console.log(data);
+        loginForm.reset();
+    }
+
+});
